a7633603ebfff565c040e9936ec092e507319476
Matteolm23 <matteo.lamalfa@davincimilazzo.edu.it>
2025-03-18 16:24:15 +0100

    Delete prodottoscalare directory

--- prodottoscalare/vettori.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-double prodscalare(double *x, double *y, size_t n)
-{
-    double prodotto = 0;
-    for(int i = 0; i < n; i++)
-        prodotto += x[i] * y[i];
-
-    return prodotto;
-}
--- prodottoscalare/vettori.h
+++
@@ -1,2 +0,0 @@
-#include<stdio.h>
-double prodscalare(double*,double*,size_t);
